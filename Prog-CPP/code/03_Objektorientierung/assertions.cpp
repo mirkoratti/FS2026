9f42b3bf406a78a78dec759d83d986adbe3cb09b
mirkoratti <mirko.ratti17@bluewin.ch>
2026-06-15 11:25:51 +0200

    add: update week 1 study session

--- Prog-CPP/code/03_Objektorientierung/assertions.cpp
+++ Prog-CPP/code/03_Objektorientierung/assertions.cpp
@@ -1,10 +1,7 @@
 //#define NDEBUG // Deaktiviert Assertions im Projekt
 #include <cassert>
 bool testStuff(int* in1){
-
-    assert(in1 != nullptr);
-    //assertion
-
-    ;//restliche implementation einer Funktion
-
+    assert(in1 != nullptr); // If in1 == nullptr, code dies
+    
+    ...
 }
--- Prog-CPP/code/03_Objektorientierung/codedocumentation.cpp
+++ Prog-CPP/code/03_Objektorientierung/codedocumentation.cpp
@@ -1,21 +1,17 @@
 /**
  * @brief Kurzzusammefassung der Klasse
- * 
  */
 class stuff{
 public:
     /**
      * @brief Funktion von x
-     * 
      */
     double x;
     /**
-     * 
      * @brief Kurzzusammefassung der Funktion
      * 
      * @param eingang Funktion des Parameter
      * @return nix, aber falls...
-     * 
      */
     void doStuff(stuff eingang);
 };
